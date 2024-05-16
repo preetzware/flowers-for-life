b17a276f9fd7a2dd266f474a85e56dfb130d438c
Preety Ramdin <devianeeramdin@outlook.com>
2024-05-16 23:24:02 +0000

    Access google worksheets and add ascii banner

--- run.py
+++ run.py
@@ -13,7 +13,60 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('flowers_for_life')
 
 flowers = SHEET.worksheet('flowers')
+garden_plants = SHEET.worksheet('garden_plants')
+houseplants = SHEET.worksheet('houseplants')
+flowerslist = SHEET.worksheet('flowerslist')
+hp_list = SHEET.worksheet('hp_list')
+gp_list = SHEET.worksheet('gp_list')
 
-data = flowers.get_all_values()
 
-print(data)
+
+def clear_screen() :
+    """
+    clears console screen.
+    """
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+
+def print_ascii_banner():
+    print(r"""
+
+
+
+ __       __)                                                      
+(, )  |  /     /)                                                  
+   | /| /  _  // _  ______    _    _/_ ___                         
+   |/ |/ _(/_(/_(__(_) // (__(/_   (__(_)                          
+   /  |                                                            
+
+""")
+
+print_ascii_banner()
+
+FLOWER = """
+
+    _
+  _(_)_                          wWWWw   _
+ (_)@(_)   vVVVv     _     @@@@  (___) _(_)_
+    (_)\    (___)   _(_)_  @@()@@   Y  (_)@(_)
+     `  |/    Y    (_)@(_)  @@@@   \|/   (_)\
+       \|    \|/    /(_)    \|      |/      |
+        | / \ | /  \|/       |/    \|      \|/
+     \\\|//\\\|/// \|///  \\\|//  \\|//  \\\|// 
+ ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+ """
+BANNER = """
+
+
+
+    ________)                                          _            
+  (, /     /)                        /)           ___/__) ,   /)   
+    /___, // ____   _  _  __  _     // _____     (, /        //  _ 
+ ) /     (/_(_) (_(/ _(/_/ (_/_)_  /(_(_)/ (_      /    _(_ /(__(/_
+(_/                               /)              (_____   /)      
+                                 (/                      )(/  
+                                
+"""
+                                      
